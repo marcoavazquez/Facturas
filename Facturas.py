50bd0ea0518bea7ba3472da34eeb3b2def2228be
Marco A. Vazquez Alonso <mrc_ant_18@hotmail.com>
2014-07-15 15:05:33 -0500

    Segundo commit

--- Facturas.py
+++ Facturas.py
@@ -1,5 +1,13 @@
 from xml.dom import minidom
 
 class Factura():
+    def __init__(self,rutaxml):
+        self.nodos = minidom.parse(rutaxml).childNodes
 
-    def getRfcEmisor(self,Fxml):
+    def RfcEmisor(self):
+        emisor = self.nodos[0].getElementsByTagName("cfdi:Emisor")
+        self.rfc_emisor = emisor[0].attributes.get("rfc").value
+        return self.rfc_emisor
+
+f = Factura("C:\Users\marco\Desktop\\facturas\AUVI890706GD2_561_CPA0809035MA.xml")
+print f.RfcEmisor()
