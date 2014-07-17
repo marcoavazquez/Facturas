049d85c6361c29b683ce22c1802d7fe568037bd0
marco <mrc_ant_18@hotmail.com>
2014-07-17 00:30:21 -0500

    Verificar listdir

--- Inicio.py
+++ Inicio.py
@@ -2,6 +2,7 @@
 
 from Facturas import Factura
 import sys
+import re
 import os
 from PySide.QtCore import *
 from PySide.QtGui import *
@@ -22,8 +23,19 @@ class main(QWidget):
         self.lbl_ruta.move(150,10)
         self.lbl_ruta.setMinimumSize(350,20)
         self.lbl_archivoscopiados = QLabel(self.mensaje,self)
-        self.lbl_archivoscopiados.move(8,60)
-        self.lbl_archivoscopiados.setMinimumSize(390,300)
+        self.lbl_archivoscopiados.move(10,60)
+        self.lbl_archivoscopiados.setMinimumSize(490,300)
+
+        self.rfcs={'CCE110310LXA':'Conev','CPA0809035MA':'CPA','CCS1103109J3':'Cseg','JIBD581227PX8':' David Jimenez Baez',
+                'FFG091112LK1':'FMG','GGO121122KQ7':'Geoperfora','VARH7808073I3':'Hector Hugo Vasquez Reyes:',
+                'IIC110711R50':'IC','GCK130220QX1':'Kuanasi','MPY110411Q22':'MTZ','PCP121025AI7':'Palenque',
+                'HECP760920GE2':'Porfirio Hernandez Cruz','QCO1302203J9':'Quanax','GCT121024N51':'Tepechiapan'}
+
+        self.meses = {'01':'01 - Enero','02':'02 - Febrero','03':'03 - Marzo','04':'04 - Abril','05':'05 - Mayo',
+                '06':'06 - Junio','07':'07 - Julio','08':'08 - Agosto','09':'09 - Septiembre','10':'10 - Octubre',
+                '11':'11 - Noviembre','12':'12 - Diciembre'}
+        self.rd = "\033[01;31m{0}\033[00m"
+        self.grn = "\033[1;36m{0}\033[00m"
 
     def run(self):
         self.show()
@@ -33,7 +45,7 @@ class main(QWidget):
         QFileDialog(self).setFileMode(QFileDialog.Directory)
         QFileDialog(self).setOption(QFileDialog.ShowDirsOnly)
         self.ruta = QFileDialog(self).getExistingDirectory(self,"Abrir Carpeta")
-        self.ruta += '\\'
+        self.ruta += '//'
         if self.ruta:
             self.lbl_ruta.setText("<b>%s</b>" % str(self.ruta))
             self.comprobarPDF()
@@ -41,29 +53,34 @@ class main(QWidget):
             self.lbl_ruta.setText("No se ha seleccionado carpeta")
 
     def copiarArchivos(self,apdf,axml):
-        rutaServ = "FacturasCopiada\\"
+        rutaServ = "FacturasCopiada//"
         fullrutaxml = self.ruta + axml
         f = Factura(fullrutaxml)
 
-        rfcs={'CCE110310LXA':'Conev','CPA0809035MA':'CPA','CCS1103109J3':'Cseg','JIBD581227PX8':' David Jimenez Baez',
-                'FFG091112LK1':'FMG','GGO121122KQ7':'Geoperfora','VARH7808073I3':'Hector Hugo Vasquez Reyes:',
-                'IIC110711R50':'IC','GCK130220QX1':'Kuanasi','MPY110411Q22':'MTZ','PCP121025AI7':'Palenque',
-                'HECP760920GE2':'Porfirio Hernandez Cruz','QCO1302203J9':'Quanax','GCT121024N51':'Tepechiapan'}
-
-        meses = {'01':'01 - Enero','02':'02 - Febrero','03':'03 - Marzo','04':'04 - Abril','05':'05 - Mayo',
-                '06':'06 - Junio','07':'07 - Julio','08':'08 - Agosto','09':'09 - Septiembre','10':'10 - Octubre',
-                '11':'11 - Noviembre','12':'12 - Diciembre'}
-
-        if not f.rfcCliente() in rfcs:
+        if not f.rfcCliente() in self.rfcs:
             cliente = "Otros"
         else:
-            cliente = rfcs[f.rfcCliente()]
+            cliente = self.rfcs[f.rfcCliente()]
 
+        rutaServ += "%s//" % cliente
+        dir_cliente = os.listdir(rutaServ)
+        re_rfc_emisor = "^%s[\W\w]+" % f.rfcEmisor()
+        patron = re.compile(re_rfc_emisor)
 
-        if os.path.exists(rutaServ + cliente + "\\" + f.rfcEmisor() + '/.+' + f.anhoFactura() + "\\" + meses[f.mesFactura()]):
+        #NO PUEDE SER DIR_CLEINTES YA QUE NO EXISTEN LAS CARPETAS , POR LO TANTO EL FOR TERMINA :/
+        for dc in dir_cliente:
+            if patron.match(dc) == None:
+                os.mkdir("%s - %s" % (f.rfcEmisor(),f.nombreEmisor()))
+                dir_emisor = "%s - %s" % (f.rfcEmisor(),f.nombreEmisor())
+                print "[][-][]",dir_emisor
+            else:
+                dir_emisor = dc
+                print "[][][]",dir_emisor
+
+        if os.path.exists("/ho9me"):
             print "si: ",f.rfcCliente()
         else:
-            print "no: ",rutaServ + cliente + "\\" + f.rfcEmisor() + '/.+' + "\\" + f.anhoFactura() + "\\" + meses[f.mesFactura()]
+            print "no: ",self.rd.format(rutaServ + cliente + "//" + f.rfcEmisor() + '/.+' + "//" + f.anhoFactura() + "//" + self.meses[f.mesFactura()])
             print f.rfcCliente()
             print apdf
 
@@ -88,7 +105,7 @@ class main(QWidget):
                     if x[len(x)-3:len(x)] == "xml":
                         if p[:len(p)-3] == x[:len(x)-3]:
                             self.copiarArchivos(p,x)
-                            print "PDF: %s -> XML: %s" % (p,x)
+                            print self.rd.format("PDF: %s -> XML: %s" % (p,x))
 
 app_main = main()
 app_main.run()
